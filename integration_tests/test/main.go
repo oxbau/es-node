168f32b97a0a24a51a7519d2dff3a267ec578c26
pingke <pingke@quarkchain.org>
2024-08-19 19:09:17 +0800

    debug 3

--- integration_tests/test/main.go
+++ integration_tests/test/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	if len(os.Args) == 0 {
+		fmt.Println("exit with null")
 		return
+	} else if len(os.Args) == 1 {
+		fmt.Println("exit with 0")
+		os.Exit(0)
 	} else {
+		fmt.Println("exit with 1")
 		os.Exit(1)
 	}
 }
