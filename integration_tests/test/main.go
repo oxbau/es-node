82dc40730a3429b8222cfdff25a8e206c5312749
pingke <pingke@quarkchain.org>
2024-08-19 19:16:34 +0800

    debug 5

--- integration_tests/test/main.go
+++ integration_tests/test/main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	fmt.Println("=======starting========")
-	if len(os.Args) == 0 {
+	if len(os.Args) == 1 {
 		fmt.Println("exit with null")
 		return
-	} else if len(os.Args) == 1 {
+	} else if len(os.Args) == 2 {
 		fmt.Println("exit with 0")
 		os.Exit(0)
 	} else {
